c29a8c49d622345ea2f82b2a54bbb2ba6a9ec1c9
manish-kumar-82 <mk8265408@gmail.com>
2025-01-30 10:59:27 +0530

    Updated server-side code

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery',false)
-const connectToMongo = async (MONGODB_URL) => {
+const connectToMongo = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/crud_application");
     console.log("Connected Successfully");
--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectToMongo = require("./db/conn");
-const userModel = require("./models/data");
-const MONGODB_URL = process.env.MONGODB_URL;
-connectToMongo(MONGODB_URL);
+connectToMongo();
 
 const router = require("./routers/data");
 app.use(cors());
